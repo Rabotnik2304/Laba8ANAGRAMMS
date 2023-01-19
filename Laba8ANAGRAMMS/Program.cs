fbb4eb1545f00a2737a68f364e6e998395e3619d
informant <artemiy.solodkin@mail.ru>
2023-01-19 12:36:48 +0500

    Final

---
+++ Laba8ANAGRAMMS/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laba8ANAGRAMMS
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] array = new string[] {"code", "doce", "ecod", "framer", "frame"};
+            
+            List<string> listOfSortedWords = new List<string>();
+
+            List<string> result = new List<string>();
+            
+            foreach (string notSortedWord in array)
+            {
+                char[] arrayOfLetters = new char[notSortedWord.Length];
+                for (int i = 0; i < notSortedWord.Length; i++)
+                {
+                    arrayOfLetters[i] = notSortedWord[i];
+                }
+                Array.Sort(arrayOfLetters);
+
+                string sortedWord = String.Join("", arrayOfLetters);
+                
+                bool isSortedWordInlistOfSortedWords = IsSortedWordInlistOfSortedWords(listOfSortedWords, sortedWord);
+
+                if (isSortedWordInlistOfSortedWords)
+                {
+                    listOfSortedWords.Add(sortedWord);
+                    result.Add(notSortedWord);
+                }
+            }
+
+            result.Sort();
+            
+            foreach (string word in result)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        private static bool IsSortedWordInlistOfSortedWords(List<string> listOfSortedWords, string sortedWord)
+        {
+            bool isSortedWordInlistOfSortedWords = true;
+            foreach (string word in listOfSortedWords)
+            {
+                if (word == sortedWord)
+                {
+                    isSortedWordInlistOfSortedWords = false;
+                }
+            }
+
+            return isSortedWordInlistOfSortedWords;
+        }
+    }
+}
